521a228d077dc401c8801abb3fa33c9a10126dfb
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-12-10 20:09:27 +0000

    [chore] strictly app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -4,9 +4,30 @@ import pkg from "./package.json";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: "SpawnPoint",
+  name: pkg.name,
   slug: "spawn-point",
   version: pkg.version,
+  scheme: "com.my_app",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+    imageWidth: 100,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: false,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+  },
   extra: {
     ...ClientEnv,
   },
